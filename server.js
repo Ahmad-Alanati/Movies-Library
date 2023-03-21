5152bc4e48838dd27d13c912dc8dfd8156e059ce
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-03-21 19:11:52 +0000

    Lab12 is done

--- server.js
+++ server.js
@@ -1,42 +1,106 @@
 'use strict';
 const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
 const movieData = require("./Movie Data/data.json");
 const app = express();
-const port = 3000;
+app.use(cors());
+const port = process.env.PORT;
+const APIKey = process.env.API_KEY
 
-function Movie(movieData){
-  this.title = movieData["title"];
-  this.posterPath = movieData["poster_path"];
-  this.overview = movieData["overview"]
+function Movie(movieData) {
+  this.id = movieData["id"];
+  this.title = movieData["title"] == null ? movieData["name"] : movieData["title"];
+  this.release_Date = movieData["release_date"] == null ? movieData["first_air_date"] : movieData["release_date"];
+  this.poster_Path = movieData["poster_path"];
+  this.overview = movieData["overview"];
 }
 
-function ResponseError(obj){
+function ResponseError(obj) {
   this.status = obj.statusCode;
-  this.responseText = this.status == "500"?"Sorry, something went wrong":"page not found error";
+  this.responseText = this.status == "500" ? "Sorry, something went wrong" : "page not found error";
 }
 
 app.get('/', serverHandler);
 app.get('/favorite', favoriteHandler);
-app.get('*', notFoundHandler);
-app.get('*', serverErrorHandler);
+app.get('/trending', trendingHandler);
+app.get('/search', searchHandler);
+app.get('/genres', genresHandler);
+app.get('/discover', discoverHandler);
+app.use('*', notFoundHandler);
 
-function notFoundHandler(req, res){
+function notFoundHandler(req, res) {
   let stat = new ResponseError(res.status(404));
   res.send(stat);
 }
-
-function serverErrorHandler(req, res){
-  let stat = new ResponseError("500");
-  res.status(500).json('stat');
-}
-
-function serverHandler(req,res){
+function serverHandler(req, res) {
   let movie = new Movie(movieData);
   res.json(movie);
 }
 
-function favoriteHandler(req,res){
+function favoriteHandler(req, res) {
   res.send("Welcome to Favorite Page");
+}
+
+function trendingHandler(req, res) {
+  let movieType = req.query.movie_Type == null ? "all" : req.query.movie_Type;
+  let movieTime = req.query.movie_Time == null ? "week" : req.query.movie_Time;
+  let url = `https://api.themoviedb.org/3/trending/${movieType}/${movieTime}?api_key=${APIKey}`;
+  //example : http://localhost:3000/trending?movie_Type=all&movie_Time=week
+  axios.get(url)
+    .then(resAPI => {
+      let arrOfTrending = resAPI.data.results.map(element => {
+        return new Movie(element);
+      });
+      res.json(arrOfTrending);
+    })
+    .catch(error => {
+      res.send(error);
+    })
+}
+
+function searchHandler(req, res) {
+  let movieName = req.query.movie_Name;
+  let url = `https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=${APIKey}`;
+  axios.get(url)
+    .then(resAPI => {
+      let arrOfMovies = resAPI.data.results.map(element => {
+        return new Movie(element);
+      });
+      res.json(arrOfMovies);
+    })
+    .catch(error => {
+      res.send(error);
+    })
+}
+
+function genresHandler(req, res) {
+  let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKey}`;
+  axios.get(url)
+    .then(resAPI => {
+      let arrOfGenres = resAPI.data.results.map(element => {
+        return new Movie(element);
+      });
+      res.json(arrOfGenres);
+    })
+    .catch(error => {
+      res.send(error);
+    })
+}
+
+function discoverHandler (req,res){
+  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${APIKey}`;
+  axios.get(url)
+    .then(resAPI => {
+      let arrOfMovies = resAPI.data.results.map(element => {
+        return new Movie(element);
+      });
+      res.json(arrOfMovies);
+    })
+    .catch(error => {
+      res.send(error);
+    })
 }
 
 app.listen(port, () => {
