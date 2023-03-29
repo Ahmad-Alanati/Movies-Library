d268fcdf8576d1085200b49d00da219fb293f99d
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-03-29 10:05:31 +0000

    fixing some problems

--- DB.sql
+++ DB.sql
@@ -3,8 +3,8 @@ CREATE DATABASE lab13;
 CREATE TABLE movies(
     id INT NOT NULL,
     title VARCHAR(255),
-    release_Date DATE,
-    poster_Path VARCHAR(500),
+    release_date DATE,
+    poster_path VARCHAR(500),
     overview VARCHAR(500),
     comment VARCHAR(500),
     PRIMARY KEY(id)
